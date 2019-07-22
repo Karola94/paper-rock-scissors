d51e535d2b144eae440888e72b4da638ba912fc9
Karolina Kuchta <linka_94@interia.eu>
2019-07-22 20:09:17 +0200

    Add params

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-const output = document.getElementById('output');
-const result = document.getElementById('result');
-const container = document.getElementById('container');
-const moves = document.querySelectorAll('.player-move');
-
 var params = {
+  output: document.getElementById('output'),
+  result: document.getElementById('result'),
+  container: document.getElementById('container'),
+  moves: document.querySelectorAll('.player-move'),
   playerScore: 0,
   computerScore: 0,
   rounds: null
@@ -31,58 +30,58 @@ function computerMove(){
 
 //Funkcja playerMove
 function playerMove(moveId){  
-  output.classList.remove('win', 'draw', 'lose');
+  params.output.classList.remove('win', 'draw', 'lose');
   var choice = computerMove();  
   
 
   if(choice === 1 && moveId == 'papier'){
-    output.innerHTML = 'DRAW <br>you played PAPER, computer also played PAPER';    
-    output.classList.add('draw');
+    params.output.innerHTML = 'DRAW <br>you played PAPER, computer also played PAPER';    
+    params.output.classList.add('draw');
   }
   else if(choice === 1 && moveId == 'kamien'){
-    output.innerHTML = 'YOU LOSE <br>you played ROCK, computer played PAPER';    
-    output.classList.add('lose');
+    params.output.innerHTML = 'YOU LOSE <br>you played ROCK, computer played PAPER';    
+    params.output.classList.add('lose');
     params.computerScore +=1;    
   }
   else if(choice === 1 && moveId == 'nozyce'){
-    output.innerHTML = 'YOU WON <br>you played SCISSORS, computer played PAPER';   
-    output.classList.add('win');
+    params.output.innerHTML = 'YOU WON <br>you played SCISSORS, computer played PAPER';   
+    params.output.classList.add('win');
     params.playerScore +=1;    
   }
   else if(choice === 2 && moveId == 'papier'){
-    output.innerHTML = 'YOU WON <br>you played PAPER, computer played ROCK';    
-    output.classList.add('win');
+    params.output.innerHTML = 'YOU WON <br>you played PAPER, computer played ROCK';    
+    params.output.classList.add('win');
     params.playerScore +=1;    
   }
   else if(choice === 2 && moveId == 'kamien'){
-    output.innerHTML = 'DRAW <br>you played ROCK, computer played ROCK';   
-    output.classList.add('draw');
+    params.output.innerHTML = 'DRAW <br>you played ROCK, computer played ROCK';   
+    params.output.classList.add('draw');
   }
   else if(choice === 2 && moveId == 'nozyce'){
-    output.innerHTML = 'YOU LOSE <br>you played SCISSORS, computer played ROCK';    
-    output.classList.add('lose');
+    params.output.innerHTML = 'YOU LOSE <br>you played SCISSORS, computer played ROCK';    
+    params.output.classList.add('lose');
     params.computerScore +=1;   
   }
   else if(choice === 3 && moveId == 'papier'){
-    output.innerHTML = 'YOU LOSE <br>you played PAPER, computer played SCISSORS';    
-    output.classList.add('lose');
+    params.output.innerHTML = 'YOU LOSE <br>you played PAPER, computer played SCISSORS';    
+    params.output.classList.add('lose');
     params.computerScore +=1;    
   }
   else if(choice === 3 && moveId == 'kamien'){
-    output.innerHTML = 'YOU WON <br>you played ROCK, computer played SCISSORS';
-    output.classList.add('win');
+    params.output.innerHTML = 'YOU WON <br>you played ROCK, computer played SCISSORS';
+    params.output.classList.add('win');
     params.playerScore +=1;    
   }
   else if(choice === 3 && moveId == 'nozyce'){
-    output.innerHTML = 'DRAW <br>you played SCISSORS, computer played SCISSORS';
-    output.classList.add('draw');
+    params.output.innerHTML = 'DRAW <br>you played SCISSORS, computer played SCISSORS';
+    params.output.classList.add('draw');
   }  
 
-  result.innerHTML = params.playerScore + " : " + params.computerScore;
+  params.result.innerHTML = params.playerScore + " : " + params.computerScore;
   
   function gameOver() {
-    moves.forEach(move => move.setAttribute('disabled', 'true'));   
-    container.classList.add('endGame');
+    params.moves.forEach(move => move.setAttribute('disabled', 'true'));   
+    params.container.classList.add('endGame');
   }
   
   function clickButtonAfterEndGame() {      
@@ -92,23 +91,70 @@ function playerMove(moveId){
   } 
   
     if(params.rounds === params.playerScore) {
-      output.innerHTML += "<br><br> YOU WON THE ENTIRE GAME!!! <br>Game over, please press the <b>New Game</b> button!";      
-      output.style.background = "#9ee585";
+      //params.output.innerHTML += "<br><br> YOU WON THE ENTIRE GAME!!! <br>Game over, please press the <b>New Game</b> button!";      
+      params.output.style.background = "#9ee585";
       gameOver();
       clickButtonAfterEndGame();             
     }
     else if(params.rounds === params.computerScore){
-      output.innerHTML += "<br><br> YOU LOST THE ENTIRE GAME :( <br>Game over, please press the <b>New Game</b> button!";
-      output.style.background = "#e25e4f";
+     //params.output.innerHTML += "<br><br> YOU LOST THE ENTIRE GAME :( <br>Game over, please press the <b>New Game</b> button!";
+      params.output.style.background = "#e25e4f";
       gameOver(); 
       clickButtonAfterEndGame();           
     } 
   
 }
 
-for(var i=0; i<moves.length; i++){
-  moves[i].addEventListener('click', function(event){  
+for(var i=0; i<params.moves.length; i++){
+  params.moves[i].addEventListener('click', function(event){  
     var dataMove = event.target.getAttribute('data-move');
     playerMove(dataMove);
   });
 }
+
+//Modals
+
+(function(){ 
+	
+	var modals = document.querySelectorAll('.modal');
+  
+  var modalLinks = document.querySelectorAll('.show-modal');
+  
+	var showModal = function(event){		
+    event.preventDefault();
+    modals.forEach(modal => modal.classList.remove('show'));            
+    var classFromHref = event.target.getAttribute('href').substr(1);
+   
+    for(var s=0; s<modals.length; s++){     
+      if(modals[s].id === classFromHref){        
+          modals[s].classList.add('show');          
+      }
+    }
+		document.querySelector('#modal-overlay').classList.add('show');
+	};
+	
+		
+	for(var i = 0; i < modalLinks.length; i++){
+		modalLinks[i].addEventListener('click', showModal);
+	}
+	
+	var hideModal = function(event){
+		event.preventDefault();
+		document.querySelector('#modal-overlay').classList.remove('show');
+	};
+	
+	var closeButtons = document.querySelectorAll('.modal .close');
+	
+	for(var i = 0; i < closeButtons.length; i++){
+		closeButtons[i].addEventListener('click', hideModal);
+	}
+	
+	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+	
+	for(var i = 0; i < modals.length; i++){
+		modals[i].addEventListener('click', function(event){
+			event.stopPropagation();
+		});
+	}
+
+})(); 
