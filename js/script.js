4e59881d880f4971192f7889beb7cf34cef1a5c0
Karolina Kuchta <linka_94@interia.eu>
2019-07-22 18:07:28 +0200

    New version

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const output = document.getElementById('output');
 const result = document.getElementById('result');
 const container = document.getElementById('container');
-var playerScore = 0;
-var computerScore = 0;
-var rounds;
+const moves = document.querySelectorAll('.player-move');
+
+var params = {
+  playerScore: 0,
+  computerScore: 0,
+  rounds: null
+}
 
 //Funkcja new game
 function askNumberOfGames() {   
@@ -14,7 +18,7 @@ function askNumberOfGames() {
     
     document.getElementById('winningGamesNumber').innerHTML = "You have " + question + " rounds.";     
   }   
-  rounds = parseInt(question); 
+  params.rounds = parseInt(question); 
   
 }
 
@@ -30,6 +34,7 @@ function playerMove(moveId){
   output.classList.remove('win', 'draw', 'lose');
   var choice = computerMove();  
   
+
   if(choice === 1 && moveId == 'papier'){
     output.innerHTML = 'DRAW <br>you played PAPER, computer also played PAPER';    
     output.classList.add('draw');
@@ -37,17 +42,17 @@ function playerMove(moveId){
   else if(choice === 1 && moveId == 'kamien'){
     output.innerHTML = 'YOU LOSE <br>you played ROCK, computer played PAPER';    
     output.classList.add('lose');
-    computerScore +=1;    
+    params.computerScore +=1;    
   }
   else if(choice === 1 && moveId == 'nozyce'){
     output.innerHTML = 'YOU WON <br>you played SCISSORS, computer played PAPER';   
     output.classList.add('win');
-    playerScore +=1;    
+    params.playerScore +=1;    
   }
   else if(choice === 2 && moveId == 'papier'){
     output.innerHTML = 'YOU WON <br>you played PAPER, computer played ROCK';    
     output.classList.add('win');
-    playerScore +=1;    
+    params.playerScore +=1;    
   }
   else if(choice === 2 && moveId == 'kamien'){
     output.innerHTML = 'DRAW <br>you played ROCK, computer played ROCK';   
@@ -56,48 +61,54 @@ function playerMove(moveId){
   else if(choice === 2 && moveId == 'nozyce'){
     output.innerHTML = 'YOU LOSE <br>you played SCISSORS, computer played ROCK';    
     output.classList.add('lose');
-    computerScore +=1;   
+    params.computerScore +=1;   
   }
   else if(choice === 3 && moveId == 'papier'){
     output.innerHTML = 'YOU LOSE <br>you played PAPER, computer played SCISSORS';    
     output.classList.add('lose');
-    computerScore +=1;    
+    params.computerScore +=1;    
   }
   else if(choice === 3 && moveId == 'kamien'){
     output.innerHTML = 'YOU WON <br>you played ROCK, computer played SCISSORS';
     output.classList.add('win');
-    playerScore +=1;    
+    params.playerScore +=1;    
   }
   else if(choice === 3 && moveId == 'nozyce'){
     output.innerHTML = 'DRAW <br>you played SCISSORS, computer played SCISSORS';
     output.classList.add('draw');
   }  
-  result.innerHTML = playerScore + " : " + computerScore;
+
+  result.innerHTML = params.playerScore + " : " + params.computerScore;
   
   function gameOver() {
-    output.innerHTML += "<br>Game over, please press the <b>New Game</b> button!";   
+    moves.forEach(move => move.setAttribute('disabled', 'true'));   
+    container.classList.add('endGame');
   }
   
-  function clickButtonAfterEndGame() {
-    document.getElementById('kamien').onclick = function() {gameOver()};
-      document.getElementById('papier').onclick = function() {gameOver()};
-      document.getElementById('nozyce').onclick = function() {gameOver()};
+  function clickButtonAfterEndGame() {      
       document.getElementById('new-game').onclick = function(){
         location.reload(true);        
       }
   } 
   
-    if(rounds === playerScore) {
-      output.innerHTML += "<br><br> YOU WON THE ENTIRE GAME!!!"
-      clickButtonAfterEndGame();
-      container.classList.add('endGame');
+    if(params.rounds === params.playerScore) {
+      output.innerHTML += "<br><br> YOU WON THE ENTIRE GAME!!! <br>Game over, please press the <b>New Game</b> button!";      
       output.style.background = "#9ee585";
+      gameOver();
+      clickButtonAfterEndGame();             
     }
-    else if(rounds === computerScore){
-      output.innerHTML += "<br><br> YOU LOST THE ENTIRE GAME :(";
-      clickButtonAfterEndGame();
-      container.classList.add('endGame');
+    else if(params.rounds === params.computerScore){
+      output.innerHTML += "<br><br> YOU LOST THE ENTIRE GAME :( <br>Game over, please press the <b>New Game</b> button!";
       output.style.background = "#e25e4f";
+      gameOver(); 
+      clickButtonAfterEndGame();           
     } 
   
+}
+
+for(var i=0; i<moves.length; i++){
+  moves[i].addEventListener('click', function(event){  
+    var dataMove = event.target.getAttribute('data-move');
+    playerMove(dataMove);
+  });
 }
